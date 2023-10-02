39eada5e9992da379f3aa17f3f5d9bdf35c250e8
alerrandro borges <alerrandrokaton@gmail.com>
2023-10-02 10:35:22 -0300

    feat: plans

--- lib/components/plans/plans.dart
+++ lib/components/plans/plans.dart
@@ -12,60 +12,145 @@ class _PlansComponentState extends State<PlansComponent> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.only(top: 50),
+      margin: const EdgeInsets.only(top: 45),
       child: Column(
         children: [
-          const Text(
-            'Escolha o melhor plano para você!',
-            style: TextStyle(color: Colors.black, fontSize: 40),
+          Container(
+            margin: const EdgeInsets.only(bottom: 45),
+            child: const Text(
+              'Escolha o melhor plano para você!',
+              style: TextStyle(color: Colors.black, fontSize: 40),
+            ),
           ),
-          SizedBox(
-            width: 1200,
-            height: 500,
+          Container(
+            width: double.infinity,
+            alignment: Alignment.center,
+            height: 1200,
             child: ListView(
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
               children: [
                 Container(
+                  padding: const EdgeInsets.only(left: 10, right: 10),
                   width: 300,
-                  padding: const EdgeInsets.only(
-                      top: 20, bottom: 20, left: 40, right: 40),
                   child: Column(
                     children: [
-                      Container(
-                        width: 300,
-                        decoration: const BoxDecoration(
-                          color: Color(0XFFF0C708),
-                        ),
-                        child: const Column(
-                          children: [
-                            Text(
-                              "PLANO START",
-                              style: TextStyle(
-                                  fontSize: 20, fontWeight: FontWeight.bold),
+                      Image.asset("plans/plano1.png"),
+                      SizedBox(
+                        height: 50,
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(
+                              const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.zero,
+                              ),
                             ),
-                            Text(
-                              "500 MEGA",
-                              style: TextStyle(
-                                  fontSize: 45, fontWeight: FontWeight.bold),
-                            )
-                          ],
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 300,
-                        child: Row(
-                          children: [
-                            Column(
-                              children: [
-                                Text("apenas"),
-                                Text("R\$"),
-                              ],
-                            )
-                          ],
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.blue),
+                          ),
+                          onPressed: () {},
+                          child: const Text(
+                            "ASSINE AGORA",
+                            style: TextStyle(color: Colors.white),
+                          ),
                         ),
                       )
                     ],
                   ),
-                )
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 10, right: 10),
+                  width: 300,
+                  child: Column(
+                    children: [
+                      Image.asset("plans/plano1.png"),
+                      SizedBox(
+                        height: 50,
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(
+                              const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.zero,
+                              ),
+                            ),
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.blue),
+                          ),
+                          onPressed: () {},
+                          child: const Text(
+                            "ASSINE AGORA",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 10, right: 10),
+                  width: 300,
+                  child: Column(
+                    children: [
+                      Image.asset("plans/plano1.png"),
+                      SizedBox(
+                        height: 50,
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(
+                              const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.zero,
+                              ),
+                            ),
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.blue),
+                          ),
+                          onPressed: () {},
+                          child: const Text(
+                            "ASSINE AGORA",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 10, right: 10),
+                  width: 300,
+                  child: Column(
+                    children: [
+                      Image.asset("plans/plano1.png"),
+                      SizedBox(
+                        height: 50,
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(
+                              const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.zero,
+                              ),
+                            ),
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.blue),
+                          ),
+                          onPressed: () {},
+                          child: const Text(
+                            "ASSINE AGORA",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
               ],
             ),
           )
